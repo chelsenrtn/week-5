60eb8ce64ca1884028a77bf40bf2d8babcf36285
chelsenrtn <chelsea.nrtn@gmail.com>
2017-03-04 12:54:22 -0800

    Add files via upload

--- trivia-game/assets/Javascript/game.js
+++ trivia-game/assets/Javascript/game.js
@@ -1,69 +1,82 @@
-$(document).ready(function()
-	{   $("#results").click(function(){
+$(document).ready(function(){ 
+	  
+	$("#results").click(function(){
 
-		if(!$("input[name=q1]:checked").val()
-		   !$("input[name=q2]:checked").val()
-		   !$("input[name=q3]:checked").val()
-		   !$("input[name=q4]:checked").val()
-		   !$("input[name=q5]:checked").val()
-		   )
-		}
+		if(!$("input[name=q1]:checked").val() &&
+		   !$("input[name=q2]:checked").val() &&
+		   !$("input[name=q3]:checked").val() &&
+		   !$("input[name=q4]:checked").val() &&
+		   !$("input[name=q5]:checked").val() 
+		   ) {
 
-		else {
+		} else {
 			var c1 = "1";
 			var c2 = "2";
 			var c3 = "3";
 			var c4 = "4";
 			var c5 = "5";
-
-	var ct1 = ($("input[name=q1]:checked".val() !='a');
-    var ct2 = ($("input[name=q2]:checked".val() !='b');
-    var ct3 = ($("input[name=q3]:checked".val() !='c');
-    var ct4 = ($("input[name=q4]:checked".val() !='d');
-    var ct5 = ($("input[name=q5]:checked".val() !='a');
+		}
+		
+		var ct1 = $("input[name=q1]:checked".val() !='a');
+    var ct2 = $("input[name=q2]:checked".val() !='b');
+    var ct3 = $("input[name=q3]:checked".val() !='c');
+    var ct4 = $("input[name=q4]:checked".val() !='d');
+    var ct5 = $("input[name=q5]:checked".val() !='a');
     var catagories = [];
-  ) 
 
-if (ct1) {catagories.push(c1) };
-if (ct2) {catagories.push(c2) };
-if (ct3) {catagories.push(c3) };
-if (ct4) {catagories.push(c4) };
-if (ct5) {catagories.push(c5) };
-
-
-var star = 'Incorect: ' + catagories.join(',')+'';
-$("#catagorylist").text(star);
-$("#catagorylist").show("slow");
-
-if (ct1) { $("#catagory1").show("slow"); };
-if (ct2) { $("#catagory1").show("slow"); };
-if (ct3) { $("#catagory1").show("slow"); };
-if (ct4) { $("#catagory1").show("slow"); };
-if (ct5) { $("#catagory1").show("slow"); };
-
-{
-	$("#closing").show("slow"); };
-
-$("#start-button").click(function(){
+		if (ct1) {catagories.push(c1) };
+		if (ct2) {catagories.push(c2) };
+		if (ct3) {catagories.push(c3) };
+		if (ct4) {catagories.push(c4) };
+		if (ct5) {catagories.push(c5) };
 
 
-		var number = 60;
+		var star = 'Incorect: ' + catagories.join(',')+'';
+		$("#catagorylist").text(star);
+		$("#catagorylist").show("slow");
 
-		alert("GO!")
+		if (ct1) { $("#catagory1").show("slow"); };
+		if (ct2) { $("#catagory1").show("slow"); };
+		if (ct3) { $("#catagory1").show("slow"); };
+		if (ct4) { $("#catagory1").show("slow"); };
+		if (ct5) { $("#catagory1").show("slow"); };
 
-	$("#start-button").on("click", start);
-	$("#submit").on("click", finish);
-	$("#restart").on("click", restart);
 
+		$("#closing").show("slow"); 
+
+		$("#start-button").click(function(){
+
+
+				var number = 60;
+
+				alert("GO!")
+
+			$("#start-button").on("click", start);
+			$("#submit").on("click", finish);
+			$("#restart").on("click", restart);
+		});
+
+
+	});
+});
+
+  $(function(){
+  $('.timer').startTimer({
+  onComplete: function(element){
+    element.addClass('is-complete');
+  },
+  loop: true,
+  loopInterval: 60,
+});
 function start(){ 
-	counter = setInterval(timer, 100);
+	counter = setInterval(timer, 60);
 	showMe(".question");
 	showMe(".answers");
 	showMe("#submit");
 }
 
 function timer() {
-	number--
+	number--;
 	$("#show-number").html("<h2>" + number + "</h2>");
 	if (number ===0){
 		alert("Time's Up!")
@@ -92,9 +105,8 @@ function restart(){
 }
 
 function hideMe(e){
-	$(e).show();
+	$(e).show()
 }
-});
 
 
 
